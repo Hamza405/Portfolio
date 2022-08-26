a44fd657f7cd7180cc6a562679b6c3a7d67de837
Hamza <hamzash863@gmail.com>
2022-08-26 11:06:48 -0400

    install tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div>Hello world</div>;
+import Head from 'next/head';
+
+export default function Home () {
+  return <div>
+    <Head>
+      <title>Hamza Al Sheikh, Front End Developer</title>
+    </Head>
+  </div>;
 }
