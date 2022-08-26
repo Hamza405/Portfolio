05da06a26a388fd5ea0fd3550fe0c6ded23dd7af
Hamza <hamzash863@gmail.com>
2022-08-26 18:27:43 -0400

    design side bar

--- components/NavBar/SideBar.tsx
+++ components/NavBar/SideBar.tsx
@@ -1,17 +1,53 @@
 import { FC } from "react";
-import { AiOutlineClose } from "react-icons/ai";
+import Link from "next/link";
+import { AiOutlineClose, AiFillLinkedin, AiFillGithub, AiFillMail } from "react-icons/ai";
 
 const SideBar: FC = () => {
-    return <div className="fixed top-0 left-0 w-full h-screen bg-black/50">
-        <div className="fixed top-0 left-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10">
-            <div className="flex w-full justify-between items-center">
-                <h2 className="text-green-800">Hamza</h2>
-                <div className="shadow-lg rounded-full p-2 shadow-gray-400 cursor-pointer">
-                    <AiOutlineClose />
+    return (
+        <div className="fixed top-0 left-0 w-full h-screen bg-black/50">
+            <div className="fixed top-0 left-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10">
+                <div className="flex w-full justify-between items-center">
+                    <h2 className="text-green-800">Hamza</h2>
+                    <div className="shadow-lg rounded-full p-2 shadow-gray-400 cursor-pointer">
+                        <AiOutlineClose />
+                    </div>
+                </div>
+                <div className="py-10 px-1 flex flex-col">
+                    <ul className="uppercase">
+                        <Link href="/">
+                            <li className="py-4">Home</li>
+                        </Link>
+                        <Link href="/">
+                            <li className="py-4">About</li>
+                        </Link>
+                        <Link href="/">
+                            <li className="py-4">Skills</li>
+                        </Link>
+                        <Link href="/">
+                            <li className="py-4">Projects</li>
+                        </Link>
+                        <Link href="/">
+                            <li className="py-4">Contact</li>
+                        </Link>
+                    </ul>
+                </div>
+                <div className="pt-10 px-1">
+                    <p className="uppercase">Let's connect</p>
+                    <div className="py-6 flex justify-start items-center w-full md:w-[80%]">
+                        <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer p-2 ">
+                            <AiFillLinkedin size={ 25 } />
+                        </div>
+                        <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer p-2 mx-8">
+                            <AiFillGithub size={ 25 } />
+                        </div>
+                        <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer p-2 ">
+                            <AiFillMail size={ 25 } />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>;
+    );
 };
 
 export default SideBar;
