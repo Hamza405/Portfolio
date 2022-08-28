9bdb9cfbd105e2544dfdae1f016ebdd656e4d0ca
Hamza <hamzash863@gmail.com>
2022-08-28 11:26:42 -0400

    Implement Button resume

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -5,10 +5,11 @@ import PersonalIdentification from "./PersonalIdentification";
 const Main: FC = () => {
     return (
         <div className="w-full h-screen text-center">
-            <div className="max-w-[1240px] w-full h-full p-2 mx-auto flex justify-center items-center" style={ { justifyContent: "center" } }>
+            <div className="max-w-[1240px] w-full h-full p-2 mx-auto flex my-8 sm:justify-center items-center">
                 <div>
                     <PersonalIdentification />
-                    <div className="py-4 flex justify-between m-auto items-center max-w-[60%]">
+                    <button className="py-4 px-8 my-4 hover:scale-105 ease-in duration-300">My Resume</button>
+                    <div className="py-4 flex justify-between m-auto items-center max-w-[70%] sm:max-w-[60%]">
                         <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300 sm:p-6 p-3 ">
                             <AiFillLinkedin size={ 25 } />
                         </div>
@@ -22,9 +23,9 @@ const Main: FC = () => {
                             <AiFillMessage size={ 25 } />
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                </div >
+            </div >
+        </div >
     );
 };
 export default Main;
