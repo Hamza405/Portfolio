414fbf41af1eb5cb5e5f4871465dca438c741a44
Hamza <hamzash863@gmail.com>
2022-08-28 11:25:44 -0400

    Improve navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -12,32 +12,26 @@ const NavBar: FC = () => {
         setSideBar( false );
     };
     return (
-        <div className="fixed shadow-xl w-full h-20 z-[100]" >
-            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-                <h2 className="text-green-800">Hamza</h2>
+        <div className="z-50 shadow-xl w-full h-20" >
+            <div className="flex justify-between items-center w-full h-full px-12 2xl: px - 16" style={ { zIndex: 50 } }>
+                < h2 className="text-green-800" > Hamza</h2 >
                 <ul className="hidden md:flex">
                     <Link href='/'>
                         <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
                     </Link>
-                    <Link href='/'>
+                    <Link href='/about'>
                         <li className="ml-10 text-sm uppercase hover:border-b">ABout</li>
                     </Link>
-                    <Link href='/'>
-                        <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
-                    </Link>
-                    <Link href='/'>
+                    <Link href='/projects'>
                         <li className="ml-10 text-sm uppercase hover:border-b">projects</li>
-                    </Link>
-                    <Link href='/'>
-                        <li className="ml-10 text-sm uppercase hover:border-b">contact</li>
                     </Link>
                 </ul>
                 <div className="md:hidden" onClick={ openSideBarHandler }>
                     <AiOutlineMenu size={ 25 } />
                 </div>
-            </div>
+            </div >
             <SideBar isOpen={ sideBar } onClose={ closeSideBarHandler } />
-        </div> );
+        </div > );
 };
 
 export default NavBar;
