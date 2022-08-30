8a3d12b67a13994aace8402b5976b5c9d8c670e1
Hamza <hamzash863@gmail.com>
2022-08-30 05:33:43 -0400

    Improve slider

--- src/components/Projects/ProjectItem.tsx
+++ src/components/Projects/ProjectItem.tsx
@@ -1,6 +1,3 @@
-import { FC } from "react";
-import Image from "next/image";
-import Link from "next/link";
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
@@ -12,41 +9,46 @@ type Item = {
 };
 const fadeImages = [
     {
-        url: '/assets/images/cv_photo.jpg',
+        url: '/assets/skills/css.png',
         caption: 'Slide 1'
     },
     {
-        url: '/assets/images/cv_photo.jpg',
+        url: '/assets/images/qlevar.png',
         caption: 'Slide 2'
     },
     {
-        url: '/assets/images/cv_photo.jpg',
+        url: '/assets/images/qlevar.jpg',
         caption: 'Slide 3'
     },
 ];
 // const ProjectItem: FC<Item> = ( props ) => {
 const ProjectItem = () => {
     return (
-        <div className="h-auto w-[500px]">
+        <div className="">
 
 
             <div className="shadow-xl shadow-gray-600
-            rounded-xl">
-                <div className="slide-container w-full mx-auto">
+            rounded-x z-10 h-auto w-[325px] sm:w-[500px] md:w-[600px]">
+                <div className="slide-container w-[100%] h-auto p-2" style={ {
+                    backgroundRepeat: 'no-repeat',
+                    minWidth: '100%', width: '100%',
+                }
+                }>
                     <Slide>
                         { fadeImages.map( ( fadeImage, index ) => (
-                            <div className="each-fade" key={ index }>
-                                <div className="image-container">
-                                    <img className="rounded-md" src={ fadeImage.url } />
+                            <div className="each-slide h-auto w-full" key={ index }>
+                                <div className="image-container flex justify-center items-center w-full h-auto">
+                                    <img style={ { objectFit: 'contain', height: 'auto' } } className="rounded-md" src={ fadeImage.url } />
                                 </div>
                             </div>
                         ) ) }
                     </Slide>
+
                     <h3 className="p-4">Qlevar</h3>
                     <p className="p-4 text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque in ducimus nam molestiae consequatur voluptate sunt, perferendis vitae facilis expedita, ipsam magnam odio suscipit, illum dicta esse mollitia aliquid placeat.</p>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,9 +4,9 @@ import ProjectItem from "./ProjectItem";
 const Projects: FC = () => {
     return (
         <div id='projects' className="w-full">
-            <div className="max-w-[1244px] p-4 mx-auto my-10 sm:py-12">
-                <p className="text-4xl text-green-800 my-4 pl-4 pb-4">What I Have Built</p>
-                <div className="grid gap-8">
+            <div className="max-w-[1244px] p-4 mx-auto my-8 sm:py-10">
+                <p className="text-4xl text-green-800 my-5 pl-4 pb-4">What I Have Built</p>
+                <div className="grid xl:grid-cols-2 gap-10">
                     <ProjectItem />
                     <ProjectItem />
                     <ProjectItem />
