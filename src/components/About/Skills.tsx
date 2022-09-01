58c808e215d29d63abf107dcdeaec5729a52bd03
Hamza405 <hamzash863@gmail.com>
2022-09-01 11:47:02 -0400

    clean up

--- src/components/About/Skills.tsx
+++ src/components/About/Skills.tsx
@@ -3,30 +3,21 @@ import SkillContainer from "./SkillContainer";
 import { Skill } from "@prisma/client";
 
 const Skills: FC<{ skills: Skill[] }> = ({ skills }) => {
+  const mainSkills = skills.filter((i) => i.basic);
+  const sideSkills = skills.filter((i) => !i.basic);
   return (
     <div className="w-full my-4">
       <p className="text-4xl text-green-800">My Skills</p>
       <div className="flex flex-wrap justify-start gap-4 md:gap-8 items-center my-4">
-        {skills.map((item) => (
+        {mainSkills.map((item) => (
           <SkillContainer key={item.id} image={item.image} />
         ))}
-
-        {/* <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" /> */}
       </div>
       <p className="text-4xl text-green-800 my-4">Familiar With</p>
       <div className="flex flex-wrap justify-start gap-4 md:gap-6 items-center my-4">
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/assets/skills/html.png" />
-        <SkillContainer image="/public/assets/skills/aws.png" />
+        {sideSkills.map((item) => (
+          <SkillContainer key={item.id} image={item.image} />
+        ))}
       </div>
     </div>
   );
