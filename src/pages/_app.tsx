8705e018230bef3ca1cbf9f95ed41ce9ce3a6ba2
Hamza405 <hamzash863@gmail.com>
2022-09-01 04:52:40 -0400

    Implement database and models

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-export default function NextApp ( { Component, pageProps }: AppProps ) {
-  return <Component { ...pageProps } />;
+export default function NextApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
-import Head from 'next/head';
-import NavBar from '../components/NavBar/NavBar';
-import Main from '../components/Main/Main';
-import About from '../components/About/About';
-import Projects from '../components/Projects/Projects';
+import Head from "next/head";
+import NavBar from "../components/NavBar/NavBar";
+import Main from "../components/Main/Main";
+import About from "../components/About/About";
+import Projects from "../components/Projects/Projects";
 
-
-export default function Home () {
-  return <div>
-    <Head>
-      <title>Hamza Al Sheikh, Front End Developer</title>
-    </Head>
-    <NavBar />
-    <Main />
-    <About />
-    <Projects />
-  </div>;
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hamza Al Sheikh, Front End Developer</title>
+      </Head>
+      <NavBar />
+      <Main />
+      <About />
+      <Projects />
+    </div>
+  );
 }
