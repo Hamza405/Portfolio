c1c3c51db4d04ab788a976eb31bff89a3a3ca690
Hamza405 <hamzash863@gmail.com>
2022-09-01 11:53:04 -0400

    Implement resume button

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,31 +1,40 @@
 import { FC } from "react";
-import { AiFillLinkedin, AiFillGithub, AiFillMail, AiFillMessage } from "react-icons/ai";
+import {
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillMail,
+  AiFillMessage,
+} from "react-icons/ai";
 import PersonalIdentification from "./PersonalIdentification";
 
 const Main: FC = () => {
-    return (
-        <div id='main' className="w-full h-screen text-center">
-            <div className="max-w-[1240px] w-full h-full p-2 mx-auto flex my-8 sm:justify-center items-center">
-                <div>
-                    <PersonalIdentification />
-                    <button className="py-4 px-8 my-4 hover:scale-105 ease-in duration-300">My Resume</button>
-                    <div className="py-4 flex justify-between m-auto items-center max-w-[70%] sm:max-w-[60%]">
-                        <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300 sm:p-6 p-3 ">
-                            <AiFillLinkedin size={ 25 } />
-                        </div>
-                        <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300 sm:p-6 p-3">
-                            <AiFillGithub size={ 25 } />
-                        </div>
-                        <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300  sm:p-6 p-3 ">
-                            <AiFillMail size={ 25 } />
-                        </div>
-                        <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300  sm:p-6 p-3 ">
-                            <AiFillMessage size={ 25 } />
-                        </div>
-                    </div>
-                </div >
-            </div >
-        </div >
-    );
+  return (
+    <div id="main" className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full p-2 mx-auto flex my-8 sm:justify-center items-center">
+        <div>
+          <PersonalIdentification />
+          <button className="py-4 px-8 my-4 hover:scale-105 ease-in duration-300">
+            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+              My Resume
+            </a>
+          </button>
+          <div className="py-4 flex justify-between m-auto items-center max-w-[70%] sm:max-w-[60%]">
+            <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300 sm:p-6 p-3 ">
+              <AiFillLinkedin size={25} />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300 sm:p-6 p-3">
+              <AiFillGithub size={25} />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300  sm:p-6 p-3 ">
+              <AiFillMail size={25} />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300  sm:p-6 p-3 ">
+              <AiFillMessage size={25} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Main;
