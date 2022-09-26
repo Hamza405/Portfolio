9160174ec4cd14b947d4e07b627c63d35e621a64
Hamza405 <hamzash863@gmail.com>
2022-09-26 10:10:41 -0400

    add links

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,6 +4,7 @@ import {
   AiFillGithub,
   AiFillMail,
   AiFillMessage,
+  AiOutlineWhatsApp,
 } from "react-icons/ai";
 import PersonalIdentification from "./PersonalIdentification";
 
@@ -18,18 +19,34 @@ const Main: FC = () => {
               My Resume
             </a>
           </button>
-          <div className="py-4 flex justify-between m-auto items-center max-w-[70%] sm:max-w-[60%]">
+          <div className="py-4 flex justify-around m-auto items-center max-w-[70%] sm:max-w-[60%]">
             <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300 sm:p-6 p-3 ">
-              <AiFillLinkedin size={25} />
+              <a
+                href="https://www.linkedin.com/in/hamza-al-sheikh"
+                target="_blank"
+              >
+                <AiFillLinkedin size={25} />
+              </a>
             </div>
             <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300 sm:p-6 p-3">
-              <AiFillGithub size={25} />
+              <a href="https://github.com/Hamza405" target="_blank">
+                <AiFillGithub size={25} />
+              </a>
             </div>
             <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300  sm:p-6 p-3 ">
-              <AiFillMail size={25} />
+              <a href="mailto: hamzash863@gmail.com" target="_blank">
+                <AiFillMail size={25} />
+              </a>
             </div>
             <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300  sm:p-6 p-3 ">
-              <AiFillMessage size={25} />
+              <a href="https://telegram.me/Hamza_Al_Sheikh" target="_blank">
+                <AiFillMessage size={25} />
+              </a>
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300  sm:p-6 p-3 ">
+              <a href="https://wa.me/+963932012299" target="_blank">
+                <AiOutlineWhatsApp size={25} />
+              </a>
             </div>
           </div>
         </div>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,48 +4,55 @@ import { AiOutlineMenu } from "react-icons/ai";
 import SideBar from "./SideBar";
 
 const NavBar: FC = () => {
-    const [ sideBar, setSideBar ] = useState( false );
-    const [ shadow, setShadow ] = useState( false );
+  const [sideBar, setSideBar] = useState(false);
+  const [shadow, setShadow] = useState(false);
 
-    const handleShadow = () => {
-        if( window.scrollY > 90 ) {
-            setShadow( true );
-        } else {
-            setShadow( false );
-        }
-    };
+  const handleShadow = () => {
+    if (window.scrollY > 90) {
+      setShadow(true);
+    } else {
+      setShadow(false);
+    }
+  };
 
-    useEffect( () => {
-        window.addEventListener( 'scroll', handleShadow );
-    }, [] );
+  useEffect(() => {
+    window.addEventListener("scroll", handleShadow);
+  }, []);
 
-    const openSideBarHandler = () => {
-        setSideBar( true );
-    };
-    const closeSideBarHandler = () => {
-        setSideBar( false );
-    };
-    return (
-        <div className={ shadow ? "sticky top-0 left-0 bg-[#ecf0f3] shadow-xl w-full h-20 z-[100]" : "sticky top-0 left-0 bg-[#ecf0f3] w-full h-20 z-[100]" }>
-            < div className="flex justify-between items-center w-full h-full px-12 2xl:px-16" >
-                < h2 className="text-green-800" > Hamza</h2 >
-                <ul className="hidden md:flex">
-                    <Link href='/'>
-                        <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
-                    </Link>
-                    <Link href='/about'>
-                        <li className="ml-10 text-sm uppercase hover:border-b">ABout</li>
-                    </Link>
-                    <Link href='/projects'>
-                        <li className="ml-10 text-sm uppercase hover:border-b">projects</li>
-                    </Link>
-                </ul>
-                <div className="md:hidden" onClick={ openSideBarHandler }>
-                    <AiOutlineMenu size={ 25 } />
-                </div>
-            </div >
-            <SideBar isOpen={ sideBar } onClose={ closeSideBarHandler } />
-        </div > );
+  const openSideBarHandler = () => {
+    setSideBar(true);
+  };
+  const closeSideBarHandler = () => {
+    setSideBar(false);
+  };
+  return (
+    <div
+      className={
+        shadow
+          ? "sticky top-0 left-0 bg-[#ecf0f3] shadow-xl w-full h-20 z-[100]"
+          : "sticky top-0 left-0 bg-[#ecf0f3] w-full h-20 z-[100]"
+      }
+    >
+      <div className="flex justify-between items-center w-full h-full px-12 2xl:px-16">
+        <h2 className="text-green-800"> Hamza</h2>
+        <ul className="hidden md:flex">
+          <Link href="/">
+            <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+          </Link>
+          <Link href="/#about">
+            <li className="ml-10 text-sm uppercase hover:border-b">ABout</li>
+          </Link>
+          <Link href="/#projects">
+            <li className="ml-10 text-sm uppercase hover:border-b">projects</li>
+          </Link>
+        </ul>
+        <div className="md:hidden" onClick={openSideBarHandler}>
+          <AiOutlineMenu size={25} />
+        </div>
+      </div>
+      <SideBar isOpen={sideBar} onClose={closeSideBarHandler} />
+    </div>
+  );
 };
 
 export default NavBar;
