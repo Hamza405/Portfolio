df3704eade9ccc941c3c1c99c9e1d9833ce7139e
Hamza405 <hamzash863@gmail.com>
2022-10-04 05:57:14 -0400

    Implement skill tooltip

--- src/components/About/SkillContainer.tsx
+++ src/components/About/SkillContainer.tsx
@@ -1,10 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Image from "next/image";
 
-const SkillContainer: FC<{ image: string }> = ({ image }) => {
+const SkillContainer: FC<{ image: string; text: string }> = ({
+  image,
+  text,
+}) => {
+  const [show, setShow] = useState(false);
   return (
-    <div className="rounded-xl shadow-lg shadow-gray-400  hover:scale-110 ease-in duration-300 p-4 flex items-center">
-      <Image src={image} width={40} height={40} />
+    <div className="relative flex flex-col">
+      {show && (
+        <div className="absolute bottom-16 left-0 opacity-75  bg-gray-600 text-white p-2 rounded-[4px] flex items-center justify-center uppercase text-sm">
+          {text}
+          <span
+            style={{
+              borderColor: "rgba(75, 85, 99, 0.9) transparent transparent",
+            }}
+            className="absolute top-[99%] left-1 border-4 border-solid"
+          />
+        </div>
+      )}
+
+      <div
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+        className=" rounded-xl shadow-lg shadow-gray-400  hover:scale-110 ease-in duration-300 p-4 flex items-center"
+      >
+        <Image src={image} width={40} height={40} />
+      </div>
     </div>
   );
 };
--- src/components/About/Skills.tsx
+++ src/components/About/Skills.tsx
@@ -10,61 +10,21 @@ const Skills: FC<{ skills: Skill[] }> = ({ skills }) => {
       <p className="text-4xl text-blue-800">My Skills</p>
       <div className="flex flex-wrap justify-start gap-4 md:gap-8 items-center my-4">
         {mainSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {mainSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {mainSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {mainSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {mainSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
+          <SkillContainer
+            key={item.id}
+            image={item.image}
+            text={item.skillName}
+          />
         ))}
       </div>
       <p className="text-4xl text-blue-800 my-6">Familiar With</p>
       <div className="flex flex-wrap justify-start gap-4 md:gap-6 items-center my-4">
         {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
-        ))}
-        {sideSkills.map((item) => (
-          <SkillContainer key={item.id} image={item.image} />
+          <SkillContainer
+            key={item.id}
+            image={item.image}
+            text={item.skillName}
+          />
         ))}
       </div>
     </div>
