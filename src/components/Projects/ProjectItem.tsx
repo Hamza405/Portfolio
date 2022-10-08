62b397502f90cd631682f89e5f2f741b059a843c
Hamza405 <hamzash863@gmail.com>
2022-10-08 11:12:36 -0400

    Improve project item

--- src/components/Projects/ProjectItem.tsx
+++ src/components/Projects/ProjectItem.tsx
@@ -17,7 +17,7 @@ const ProjectItem: FC<{ project: Project }> = ({ project }) => {
   return (
     <div
       className="shadow-xl shadow-gray-600
-            rounded-md  hover:scale-105 ease-in duration-300 z-10 h-auto w-[325px] sm:w-[500px] md:w-[600px]"
+            rounded-md  hover:scale-105 ease-in duration-300 z-10 h-auto w-[325px] sm:w-[500px] md:w-[600px] bg-gray-300"
     >
       <div
         className="slide-container w-[100%] h-auto p-2"
@@ -29,8 +29,8 @@ const ProjectItem: FC<{ project: Project }> = ({ project }) => {
       >
         <Slide key={project.id}>
           {images.map((fadeImage, index) => (
-            <div className="each-slide h-auto w-full" key={index}>
-              <div className="image-container flex justify-center items-center w-full h-auto">
+            <div className="each-slide h-auto w-auto" key={index}>
+              <div className="image-container flex justify-center items-center w-auto h-auto">
                 <img
                   style={{ objectFit: "contain", height: "auto" }}
                   className="rounded-md"
@@ -41,22 +41,40 @@ const ProjectItem: FC<{ project: Project }> = ({ project }) => {
           ))}
         </Slide>
         <div className="flex justify-between items-center">
-          <div
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
-            className="flex items-center p-4 text-blue-600 cursor-pointer"
+          <a
+            href={project.projectLink}
+            target="_blank"
+            className={
+              project.projectLink
+                ? "cursor-pointer"
+                : "cursor-default pointer-events-none"
+            }
           >
-            <h3>{project.projectName}</h3>
-            <div className="pt-2 pl-2">
-              <MdOutlineNavigateNext
-                size={25}
-                className={hover ? "animate-ping" : ""}
-              />
+            <div
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+              className="flex items-center p-4 text-blue-600"
+            >
+              <h3>{project.projectName}</h3>
+              {project.projectLink && (
+                <div className="pt-2 pl-2">
+                  <MdOutlineNavigateNext
+                    size={25}
+                    className={hover ? "animate-ping" : ""}
+                  />
+                </div>
+              )}
             </div>
-          </div>
-          <button className="px-2 py-1 text-sm mt-2 mr-4 sm:px-4 sm:py-2 shadow-xl shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-[#5651e5] to-[#709dff] text-white hover:scale-110 ease-in duration-300">
-            View Code
-          </button>
+          </a>
+          {project.projectCodeLink && (
+            <a
+              href={project.projectCodeLink}
+              target="_blank"
+              className="px-2 py-1 text-sm mt-2 mr-4 sm:px-4 sm:py-2 shadow-xl shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-[#5651e5] to-[#709dff] text-white hover:scale-110 ease-in duration-300 cursor-pointer"
+            >
+              View Code
+            </a>
+          )}
         </div>
         <p className="p-4 text-md">{project.projectDescription}.</p>
       </div>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -6,12 +6,9 @@ const Projects: FC<{ projects: Project[] }> = ({ projects }) => {
   return (
     <div id="projects" className="max-w-[1244px] p-4 mx-auto py-10 sm:py-12">
       <p className="text-4xl text-blue-800 my-5 pl-4 pb-4">What I Have Built</p>
-      <div className="grid justify-center items-center xl:grid-cols-2 gap-10">
+      <div className="flex flex-col justify-center items-center gap-10">
         {projects.map((project) => (
           <ProjectItem key={project.id} project={project} />
-        ))}
-        {projects.map((project) => (
-          <ProjectItem key="asdas" project={project} />
         ))}
       </div>
     </div>
