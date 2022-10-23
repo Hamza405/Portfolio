75af5f70f3787fc467e1c6e0be3ab48538ae7849
Hamza405 <hamzash863@gmail.com>
2022-10-23 16:35:19 -0400

    responsive

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import Image from "next/image";
 import Skills from "./Skills";
 import { Skill } from "@prisma/client";
@@ -10,6 +10,17 @@ const About: FC<{ skills: Skill[] }> = ({ skills }) => {
       className="flex w-full flex-wrap justify-between items-center p-4 mx-auto my-12 sm:my-14"
     >
       <div className="py-4 max-w-[85%] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-1 flex justify-center items-start pt-2 sm:pt-10 md:hidden">
+          <div className="rounded-xl shadow-lg shadow-gray-400 p-2 bg-gray-500 max-w-[350px]">
+            <Image
+              className="rounded-xl m-auto"
+              src="/assets/images/cv_photo.jpg"
+              alt="Hamza Al Sheikh"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
         <div className="col-span-2">
           <p className="text-4xl text-blue-800 my-4">About me</p>
           <p className="text-lg">
@@ -32,7 +43,8 @@ const About: FC<{ skills: Skill[] }> = ({ skills }) => {
           </p>
           <Skills skills={skills} />
         </div>
-        <div className="col-span-1 flex justify-center items-start pt-2 sm:pt-10">
+
+        <div className="col-span-1 md:flex justify-center items-start pt-2 hidden ">
           <div className="rounded-xl shadow-lg shadow-gray-400 p-2 bg-gray-500 max-w-[350px]">
             <Image
               className="rounded-xl m-auto"
--- src/components/About/Skills.tsx
+++ src/components/About/Skills.tsx
@@ -8,7 +8,7 @@ const Skills: FC<{ skills: Skill[] }> = ({ skills }) => {
   return (
     <div className="w-full my-4">
       <p className="text-4xl text-blue-800">My Skills</p>
-      <div className="flex flex-wrap justify-start gap-4 md:gap-8 items-center my-4">
+      <div className="flex flex-wrap justify-center md:justify-start gap-4 md:gap-8 items-center my-4">
         {mainSkills.map((item) => (
           <SkillContainer
             key={item.id}
@@ -18,7 +18,7 @@ const Skills: FC<{ skills: Skill[] }> = ({ skills }) => {
         ))}
       </div>
       <p className="text-4xl text-blue-800 my-6">Familiar With</p>
-      <div className="flex flex-wrap justify-start gap-4 md:gap-6 items-center my-4">
+      <div className="flex flex-wrap justify-center md:justify-start gap-4 md:gap-6 items-center my-4">
         {sideSkills.map((item) => (
           <SkillContainer
             key={item.id}
