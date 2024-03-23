bbfa0b3e0c79c9d234cefb2314d79fb4817c400c
Hamza Al Sheikh <hamza@Hamzas-MacBook-Pro.local>
2024-03-23 22:46:33 +0000

    clean

--- src/components/Main/PersonalIdentification.tsx
+++ src/components/Main/PersonalIdentification.tsx
@@ -4,13 +4,12 @@ const PersonalIdentification: FC = () => {
   return (
     <>
       <h1 className="py-4 text-gray-800">
-        Hi, I'm <span className="text-indigo-600">Hamza</span>
+        Hello, I'm <span className="text-indigo-600">Hamza</span>
       </h1>
-      <h2 className="py-2 text-gray-800">A Front-End Developer</h2>
+      <h2 className="py-2 text-gray-800">Software Engineer</h2>
       <p className="py-4 text-gray-600 text-md max-w-[70%] mx-auto">
-        I'm Fresh graduate computer engineer, Front-End developer building
-        responsive web and mobile application,while I am learning back-end
-        technologies
+        three years of frontend and one year of full-stack development
+        experience
       </p>
     </>
   );
