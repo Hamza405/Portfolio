75ac58b7a2e6910a09171d928ea76d55313a506a
Hamza405 <hamzash863@gmail.com>
2024-04-10 23:22:48 +0100

    build

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -17,6 +17,7 @@ const Seo: FC = () => {
       <meta property="og:site_name" content={siteName} />
       <meta property="og:description" content={description} />
       <meta property="og:title" content={siteName} />
+      <meta name="twitter:card" content="summary_large_image" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <meta property="og:url" content="https://hamzaalsheikh.vercel.app" />
     </Head>
